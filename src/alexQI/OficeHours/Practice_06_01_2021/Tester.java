1e267b263c4ebb3606e93e0c0c7b069452994a83
Dst07032023 <bovari.deonisis@gmail.com>
2023-10-28 16:25:24 +0200

    tester

---
+++ src/alexQI/OficeHours/Practice_06_01_2021/Tester.java
@@ -0,0 +1,33 @@
+package alexQI.OficeHours.Practice_06_01_2021;
+
+/*
+- create a class called Tester
+        Attributes:
+            name, employeeID, JobTitle, Salary
+
+        Constructor:
+            accepts and sets all instance variables
+
+        Actions:
+            getter/setters, smokeTesting(), creatingTicket(), toString()
+
+        -> Optional: static variable
+
+ */
+
+public class Tester {
+
+    private String name;
+    private int employeeID;
+    private String jobTitle;
+    private double salary;
+
+    public Tester(String name, int employeeID, String jobTitle, double salary) {
+
+    }
+
+    public static void main(String[] args) {
+        Tester test = new Tester();
+    }
+
+}
