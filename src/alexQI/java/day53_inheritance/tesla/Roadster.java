f78a0bebcb8c2f612e6bce221fe5fea668b8cc8a
Dst07032023 <bovari.deonisis@gmail.com>
2023-11-04 18:39:38 +0100

    - added ModelX.java sub class and instantiated

--- src/alexQI/java/day53_inheritance/tesla/Roadster.java
+++ src/alexQI/java/day53_inheritance/tesla/Roadster.java
@@ -2,8 +2,10 @@ package alexQI.java.day53_inheritance.tesla;
 
 public class Roadster extends ElectricCar {
 
-    public Roadster(String make, String model, double price, int year, int range) {
-        super(make, model, price, year, range);
+    //Roadster r = new Roadster("Roadster", 200000, 2022, 620);
+    public Roadster(String model, double price, int year, int range) {
+        super("Tesla", model, price, year, range);
+        System.out.println("Welcome new Roadster!");
     }
 }
 
