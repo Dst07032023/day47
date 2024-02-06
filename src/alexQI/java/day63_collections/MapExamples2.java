ea10bd6923a133529ec57a9af426e78204da976d
Dst07032023 <bovari.deonisis@gmail.com>
2024-02-06 14:50:39 +0100

    Collections 2

--- src/alexQI/java/day63_collections/MapExamples2.java
+++ src/alexQI/java/day63_collections/MapExamples2.java
@@ -29,7 +29,35 @@ public class MapExamples2 {
         Map<Integer, Person> map3 = new TreeMap<>();
         map3.put(32, new Person("MJ", 32));
         map3.put(0, new Person("", 0));
-        map3.put(12, new Person("Nick", 10));
+        map3.put(-12, new Person("Nick", 10));
         System.out.println(map3);
+
+        /*
+        DB EXAMPLES:
+
+        FIRST_NAME James
+        LAST_NAME Bond
+        AGE 50
+
+        DB
+        FIRST_NAME Amy
+        LAST_NAME Rose
+        AGE 30
+
+        Map<String, String> map = new HashMAp<>();
+        map.put("FIRST_NAME", "James");
+        map.put("LAST_NAME", "Bond");
+
+        map.get("FIRST_NAME")
+
+        if(map.get("AGE") < 0) fail test case
+
+        if(map.get("FIRST_NAME") == null) fail test case
+
+        List<Map<String, String> > list
+
+            list.get(0) -> Map<String, String> (james bond)
+            list.get(0).get("FIRST_NAME") -> james
+         */
     }
 }
